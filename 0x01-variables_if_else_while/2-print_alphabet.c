edba97044d8049023e9f23abe0f29de5d2f08d8d
root <kebedemathiyos@gmail.com>
2022-03-14 14:17:00 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
