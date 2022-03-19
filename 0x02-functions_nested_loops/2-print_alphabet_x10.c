9d0af4a3d0c75e3381503467de9bdf3803661d09
Mathiyos <99494928+Mathiyos@users.noreply.github.com>
2022-03-19 23:40:04 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,35 @@
+alx-low_level_programming/0x02-functions_nested_loops/2-print_alphabet_x10.c
+@em432
+em432 my commit message
+Latest commit 7be7d1d 3 days ago
+ History
+ 1 contributor
+Executable File  27 lines (21 sloc)  288 Bytes
+   
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet lowercase 10 times
+ * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
-
 void print_alphabet_x10(void)
 {
-  
-char alphabet;
-int i;
-for (i = 0; i < 10; i++){
+int alphabet;
+int count;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+count = 0;
+while (count < 10)
 {
-  
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
 _putchar(alphabet);
-
 }
 
+count++;
 _putchar('\n');
 }
-  
-}
 
+
+}
